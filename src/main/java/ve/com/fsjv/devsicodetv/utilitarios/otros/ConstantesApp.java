94b7eede610492e9a55a74b0eb4fedbaa2626ca9
Franklin Sarmiento <fgsc.soluciones@gmail.com>
2013-11-01 12:58:10 -0430

    (FS) - Actualizando la clase ConstantesApp.java

--- src/main/java/ve/com/fsjv/devsicodetv/utilitarios/otros/ConstantesApp.java
+++ src/main/java/ve/com/fsjv/devsicodetv/utilitarios/otros/ConstantesApp.java
@@ -24,10 +24,30 @@ public class ConstantesApp {
     public static final String ACRONIMO_MODULO_ORGANISMO = "ORG";
     public static final String ACRONIMO_MODULO_EVIDENCIAS = "EVI";
     
+    public static final String APLICACION = "Sicodet v2.0";
+    
+    public static final String MODULO_FICHA_DETENIDO = "Ficha de Detenido";
+    public static final String MODULO_SALIDA = "Salidas";
+    public static final String MODULO_TRASLADOS = "Traslados";
+    public static final String MODULO_REGISTRAR_CAUSA = "Causas de Detencion";
+    public static final String MODULO_NIVEL_CALABOZO = "Calabozos";
+    public static final String MODULO_CELDA = "Celdas";
+    public static final String MODULO_TIPO_SALIDA = "Tipo de Salidas";
+    public static final String MODULO_SUB_TIPO_SALIDA = "Sub Tipo de Salida";
+    public static final String MODULO_TIPO_TRASLADO = "Tipo de Traslados";
+    public static final String MODULO_SUB_TIPO_TRASLADOS = "Sub Tipo de Traslados";
+    public static final String MODULO_ASIGNACION_CELDA = "Asignacion/Cambio de Celda";
+    public static final String MODULO_ORGANISMO = "Organismos";
+    public static final String MODULO_EVIDENCIAS = "Evidencias";
+    public static final String MODULO_HISTORIALES_RELACIONADOS = "Historiales Relacionados";
+    public static final String MODULO_ULTIMOS_AGREGADOS = "Ultimos Registros Agregados";
+    public static final String MODULO_BUSQUEDAS_DETENIDOS = "Busqueda de Detenido";
+    
     public static final boolean NO_EDITABLE = false;
     public static final boolean EDITABLE = true;
     public static final String EN_BLANCO = "";
     
-    
+    public static final String FORMATO_FECHA = "dd/mm/YY";
+    public static final String FORMATO_FECHA_COMPLETA = "dd/mm/YY H:i:s";
     
 }
