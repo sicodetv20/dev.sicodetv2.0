6a34f825731449b8ef29f5203db01622c0350516
Franklin Sarmiento <fgsc.soluciones@gmail.com>
2013-11-03 00:50:25 -0430

    (FS) #29 #30 #31 Creando Excepciones, ajustando Procesos.java y ConstantesApp.java

---
+++ src/main/java/ve/com/fsjv/devsicodetv/utilitarios/excepciones/ExcepcionMinLength.java
@@ -0,0 +1,22 @@
+package ve.com.fsjv.devsicodetv.utilitarios.excepciones;
+/**
+ *
+ * @author franklin
+ */
+public class ExcepcionMinLength extends Exception {
+    /**
+     * Creates a new instance of
+     * <code>ExcepcionMinLength</code> without detail message.
+     */
+    public ExcepcionMinLength() {
+    }
+    /**
+     * Constructs an instance of
+     * <code>ExcepcionMinLength</code> with the specified detail message.
+     *
+     * @param msg the detail message.
+     */
+    public ExcepcionMinLength(String msg) {
+        super(msg);
+    }
+}
--- src/main/java/ve/com/fsjv/devsicodetv/utilitarios/otros/ConstantesApp.java
+++ src/main/java/ve/com/fsjv/devsicodetv/utilitarios/otros/ConstantesApp.java
@@ -46,6 +46,7 @@ public class ConstantesApp {
     public static final boolean NO_EDITABLE = false;
     public static final boolean EDITABLE = true;
     public static final String EN_BLANCO = "";
+    public static final String INICIALIZAR_STRING = null;
     
     public static final String FORMATO_FECHA = "dd/MM/yyyy";
     public static final String FORMATO_FECHA_COMPLETA = "dd/MM/yyyy HH:mm:ss";
@@ -61,12 +62,22 @@ public class ConstantesApp {
     public static final int TIPO_VALIDACION_HORA = 6;
     public static final int TIPO_VALIDACION_NUMERICO = 7;
     
-    public static final int BACKGROUND_ERROR_R = 255;
-    public static final int BACKGROUND_ERROR_G = 213;
-    public static final int BACKGROUND_ERROR_B = 213;
+    public static final float BACKGROUND_ERROR_R = 255;
+    public static final float BACKGROUND_ERROR_G = 213;
+    public static final float BACKGROUND_ERROR_B = 213;
     
-    public static final int BORDER_ERROR_R = 255;
-    public static final int BORDER_ERROR_G = 0;
-    public static final int BORDER_ERROR_B = 0;
+    public static final int BANDERA_TRUE = 1;
+    public static final int BANDERA_FALSE = 0;
+    
+    public static final String MENSAJE_CAMPO_VACIO = "Error 001: Campo vacio, componente referenciado: ";
+    public static final String MENSAJE_CAMPO_EXCEDE_MAXLENGTH = "Error 002: El length del campo excede la cantidad de " + MAXLENGTH + " Caracteres, componente referenciado: ";
+    public static final String MENSAJE_CAMPO_MENOR_MINLENGTH = "Error 003: El length del campo es menor de " + MINLENGTH + "Caracteres, componente referenciado: ";
+    public static final String MENSAJE_CAMPO_FECHA_INVALIDO = "Error 004: Formato Fecha Simple Invalido, componente referenciado: ";
+    public static final String MENSAJE_CAMPO_FECHA_COMPLETA_INVALIDO = "Error 005: Formato de Fecha larga invalido, componente referenciado: ";
+    public static final String MENSAJE_CAMPO_HORA_INVALIDO = "Error 006: Formato de Hora invalido, componente referenciado: ";
+    public static final String MENSAJE_CAMPO_NUMERICO_INVALIDO = "Error 007: Campo no es numerico, componente referenciado: ";
+    public static final String MENSAJE_COMPONENTE_NULO = "Error 008: Componente nulo.";
+    public static final String MENSAJE_CHECKBOX_NO_SELECCIONADO = "Error 009: Item No Seleccionado, componente referenciado: ";
+    public static final String MENSAJE_PASSWORD_DIFERENTES = "Error 010: Contraseñas no coinciden.";
     
 }
